6401c5a2c4c5b0bcd21fe15072804f285535c84b
GcobaniDike <60971780+GcobaniDike@users.noreply.github.com>
2020-02-19 12:10:36 +0200

    Add files via upload

---
+++ Query.sql
@@ -0,0 +1,6 @@
+select * from mydatabase.data_valt;
+select Chips,Quantity,Price from mydatabase.data_valt;
+select Cooldrinks,Quantity,Price from mydatabase.data_valt;
+select Pies,Quantity,Price from mydatabase.data_valt;
+select Fruit,Quantity,Price from mydatabase.data_valt;
+select Veggies,Quantity,Price from mydatabase.data_valt;
